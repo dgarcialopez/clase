8587a1b876e2095f3e99b795ddbec4b38a25c010
d.garcia_0487@hotmail.com <d.garcia_0487@hotmil.com>
2018-03-07 13:22:13 -0800

    Archivo Hello world añadido por primera vez.

---
+++ src/com/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//mostrar "hola mundo"
+		System.out.println("hola mundo");
+
+	}
+
+}
+
+
